96917836a4bf9fd7e08222a26c2b1d67a1cf8227
Misha Kalienchenko <blvckwhite007@gmail.com>
2024-11-09 20:31:59 +0200

    Add files via upload

---
+++ Lab1/FirstWay/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int b = 9;
+    int c = 2;
+    int a = b / c;
+    printf("%d\n", a);
+    return 0;
+}
---
+++ Lab1/SecondWay/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    float x;
+    float y;
+    printf("Type your X:");
+    scanf("%f",&x);
+    if ( 0 < x && x <= 5)
+    {
+        y = x*x*x - 5*x*x;
+        printf("Result = %f",y);
+    }
+    else
+        if (-32 <= x && x < -20 || x > 10)
+        {
+            y = x*x - 3;
+            printf("Result = %f",y);
+        }
+        else
+        {
+            printf("No solution :(");
+        }
+}
